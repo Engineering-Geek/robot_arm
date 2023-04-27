4ecb306e2669291d25b78debff0ca9d2aab16bb8
Nikhil Melgiri <nmelgiri@uwaterloo.ca>
2023-04-27 00:30:50 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'robot_arm'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools', 'rclpy', 'mujoco'],
+    zip_safe=True,
+    maintainer='Nikhil Melgiri',
+    maintainer_email='melgirinik@gmail.com',
+    description='TODO: Package description',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
